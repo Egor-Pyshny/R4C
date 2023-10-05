8749abb2fafd099248fb14cd19b36bbb2e68aa25
Egor251 <egorpyshny@gmail.com>
2023-10-05 18:29:03 +0300

    add order validation

--- utils/validators.py
+++ utils/validators.py
@@ -1,15 +1,34 @@
 import json
 from datetime import datetime
+from re import match
 from typing import Dict
 from typing import List
 
 from django.core.handlers.wsgi import WSGIRequest
 
+from utils.custom_exceptions.MailFormatException import MailFormatException
 from utils.custom_exceptions.MultiException import MultiException
 from utils.custom_exceptions.NotEnoughDataException import (
     NotEnoughDataException,
 )
+from utils.custom_exceptions.SerialFormatException import SerialFormatException
 from utils.custom_exceptions.WrongLengthException import WrongLengthException
+
+
+def validate_order_data(request: WSGIRequest) -> Dict[str, str]:
+    data: Dict[str, str] = json.loads(request.body)
+    exceptions: List[BaseException] = []
+    if not (email := data.get("email")):
+        exceptions.append(NotEnoughDataException("email"))
+    elif "@" not in email:
+        exceptions.append(MailFormatException())
+    if not (serial := data.get("serial")):
+        exceptions.append(NotEnoughDataException("serial"))
+    elif not (match(r"^[A-Za-z0-9]{2}-[A-Za-z0-9]{2}$", serial)):
+        exceptions.append(SerialFormatException())
+    if len(exceptions) != 0:
+        raise MultiException(exceptions)
+    return data
 
 
 def validate_robot_data(request: WSGIRequest) -> Dict[str, str]:
